bcf7629e2b3d7bae51f584b1160ac1298d0fe482
luiz felipe <luizfelipebragaa@gmail.com>
2023-03-17 22:45:15 -0300

    feat - adding react-query dev tools

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { MainComponent } from "@components/Main";
 import { GlobalStyle } from "./global";
 
@@ -7,6 +8,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <GlobalStyle />
       <MainComponent />
     </QueryClientProvider>
